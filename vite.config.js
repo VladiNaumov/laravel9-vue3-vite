bc660f57d5e432343617fae81a92b0d0ab0ecd67
NaumDeveloper <vov4.games@yandex.ru>
2023-07-08 13:22:51 +0300

    desks

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import {defineConfig} from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vuePlugin from '@vitejs/plugin-vue'
+import * as path from "path";
 
 export default defineConfig({
     plugins: [
@@ -13,4 +14,11 @@ export default defineConfig({
             refresh: true,
         }),
     ],
+
+   resolve: {
+      alias: {
+         '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
+      }
+   },
+
 });
