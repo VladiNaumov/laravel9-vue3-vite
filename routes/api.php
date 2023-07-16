8f1141fe41fbdc24d282f2f52842b00567664274
NaumDeveloper <vov4.games@yandex.ru>
2023-07-16 19:50:35 +0300

    refactoring

--- routes/api.php
+++ routes/api.php
@@ -21,4 +21,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 
-Route::apiResource('desks', DeskController::class);
+Route::apiResources([
+    'desks' => DeskController::class,
+    //'posts' => PostController::class,
+]);
